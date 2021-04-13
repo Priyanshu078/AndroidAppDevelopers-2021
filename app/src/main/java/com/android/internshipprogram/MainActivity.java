195ea9456534bed8922101e46257d2b9694ecce4
Priyanshu Paliwal <66347715+Priyanshu078@users.noreply.github.com>
2021-04-13 22:11:51 +0530

    Program for custom toolbar

--- app/src/main/java/com/android/internshipprogram/MainActivity.java
+++ app/src/main/java/com/android/internshipprogram/MainActivity.java
@@ -1,14 +1,23 @@
 package com.android.internshipprogram;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    @SuppressLint("WrongConstant")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.custom_layout);
+
+
     }
 }
